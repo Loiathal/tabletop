7ee91c48a25a239b7a289aa7877a45604a7f0f67
unknown <zmcanally@.infoworks-tn.com>
2011-10-07 19:05:49 -0500

    Very first commit. Includes Bryan's file.

---
+++ src/tabletop/CoreCharacter.java
@@ -0,0 +1,199 @@
+package tabletop;
+
+public class CoreCharacter {
+    
+    String name;
+    String race;
+    String classlvl;
+    int ECL;
+    String size;
+    int reach;
+    String favoredclass;
+    int str;
+    int dex;
+    int con;
+    int wis;
+    int Int;
+    int cha;
+    int HP;
+    int fortsave;
+    int refsave;
+    int willsave;
+    int BAB;
+    int regAC;
+    int flatAC;
+    int touchAC;
+    int flattouchAC;
+    int charweight;
+    int itemweight;
+    int totalweight;
+    int movespeed;
+    String notes;
+    
+    // things Byran isn't sure how to code: skills, feats, items, spells, languages, status, companion, special
+    // should weapon proficencies be considered part of feats?
+    
+    void getname(){
+        
+    }
+    void setname(){
+        
+    }
+    void getrace(){
+        
+    }
+    void setrace(){
+        
+    }
+    void getclasslvl(){
+        
+    }
+    void setclasslvl(){
+        
+    }
+    void getECL(){
+        
+    }
+    void setECL(){
+        
+    }
+    void getsize(){
+        
+    }
+    void setsize(){
+        
+    }
+    void getreach(){
+        
+    }
+    void setreach(){
+        
+    }
+    void getfavoredclass(){
+        
+    }
+    void setfavoredclass(){
+        
+    }
+    void getstr(){
+    
+    }
+    void setstr(){
+        
+    }
+    void getdex(){
+        
+    }
+    void setdex(){
+        
+    }
+    void getcon(){
+        
+    }
+    void setcon(){
+        
+    }
+    void getwis(){
+        
+    }
+    void setwis(){
+        
+    }
+    void getInt(){
+        
+    }
+    void setInt(){
+        
+    }
+    void getcha(){
+        
+    }
+    void setcha(){
+        
+    }
+    void getHP(){
+        
+    }
+    void setHP(){
+        
+    }
+    void getfortsave(){
+        
+    }
+    void setfortsave(){
+        
+    }
+    void getrefsave(){
+        
+    }
+    void setrefsave(){
+        
+    }
+    void getwillsave(){
+        
+    }
+    void setwillsave(){
+        
+    }
+    void getBAB(){
+        
+    }
+    void setBAB(){
+        
+    }
+    void getAC(){
+        
+    }
+    void setAC(){
+        
+    }
+    void getflatAC(){
+        
+    }
+    void setflatAC(){
+        
+    }
+    void gettouchAC(){
+        
+    }
+    void settouchAC(){
+        
+    }
+    void getflattouchAC(){
+        
+    }
+    void setflattouchAC(){
+        
+    }
+    void getcharweight(){
+        
+    }
+    void setcharweight(){
+        
+    }
+    void getitemweight(){
+        
+    }
+    void setitemweight(){
+        
+    }
+    void gettotalweight(){
+        
+    }
+    void settotalweight(){
+        
+    }
+    void getmovespeed(){
+        
+    }
+    void setmovespeed(){
+        
+    }
+    void getnotes(){
+    
+    }
+    void setnotes(){
+        
+    }
+}
+    
