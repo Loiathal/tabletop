ad23e52e8c915747c246915428260d16511a877a
unknown <Hashiikun@.(none)>
2011-10-09 17:44:49 -0500

    finished removing void set and get's

--- src/tabletop/CoreCharacter.java
+++ src/tabletop/CoreCharacter.java
@@ -2,185 +2,155 @@ package tabletop;
 
 public class CoreCharacter {
     
-    String name;
-    String race;
-    String classlvl;
-    int ECL;
-    String size;
-    int reach;
-    String favoredclass;
-    private int Str;
-    private int Dex;
-    private int Con;
-    private int Wis;
-    private int Int;
-    private int Cha;
-    int HP;
-    int fortsave;
-    int refsave;
-    int willsave;
-    int BAB;
-    int regAC;
-    int flatAC;
-    int touchAC;
-    int flattouchAC;
-    int charweight;
-    int itemweight;
-    int totalweight;
-    int movespeed;
-    String notes;
+    private String name;
+    private String race;
+    private String classlvl;
+    private int ECL;
+    private String size;
+    private int reach;
+    private String favoredClass;
+    private int _str;
+    private int _dex;
+    private int _con;
+    private int _wis;
+    private int _int;
+    private int _cha;
+    private int HP;
+    private int fortsave;
+    private int refsave;
+    private int willsave;
+    private int BAB;
+    private int characterweight;
+    private int movespeed;
+    private String notes;
     
     // things Byran isn't sure how to code: skills, feats, items, spells, languages, status, companion, special
     // should weapon proficencies be considered part of feats?
     
-    void getname(){
-        
-    }
-    void setname(){
-        
-    }
-    void getrace(){
-        
-    }
-    void setrace(){
-        
-    }
-    void getclasslvl(){
-        
-    }
-    void setclasslvl(){
-        
-    }
-    void getECL(){
-        
-    }
-    void setECL(){
-        
-    }
-    void getsize(){
-        
-    }
-    void setsize(){
-        
-    }
-    void getreach(){
-        
-    }
-    void setreach(){
-        
-    }
-    void getfavoredclass(){
-        
-    }
-    void setfavoredclass(){
-        
+    public String getCharacterName(String characterName){
+        if (characterName.equalsIgnoreCase("name")){
+            return name;
+        } else {
+            return null;
+        }
     }
     
+    public String getCharacterRace(String characterRace){
+        if (characterRace.equalsIgnoreCase("race")){
+            return race;
+        } else {
+            return null;
+        }
+    }
+    
+    public String getclasses(String classes){
+        if (classes.equalsIgnoreCase("class") || classes.equalsIgnoreCase("classlevel") || classes.equalsIgnoreCase("classlvl")){
+            return classlvl;
+        } else {
+            return null;
+        }
+    }
+
+    public int getEffectiveCharacterLevel(String effectiveCharacterLevel){
+        if (effectiveCharacterLevel.equalsIgnoreCase("ecl") || effectiveCharacterLevel.equalsIgnoreCase("effectivecharacterlevel") || effectiveCharacterLevel.equalsIgnoreCase("effectivecharacterlvl")){
+            return ECL;
+        } else {
+            return -1;
+        }
+    }
+
+    public String getSizeCategory (String sizeCategory){
+        if (sizeCategory.equalsIgnoreCase("size") || sizeCategory.equalsIgnoreCase("sizecategory")){
+            return size;
+        } else {
+            return null;
+        }
+    }
+    
+    public int getRange (String range){
+        if (range.equalsIgnoreCase("reach")){
+            return reach;
+        } else {
+            return -1;
+        }
+    }
+    
+    public String getFavoriteClass (String favoriteClass){
+        if (favoriteClass.equalsIgnoreCase("favoredclass")){
+            return favoredClass;
+        } else {
+            return null;
+        }
+    }
+
     public int getAbilityScore(String abilityScore){
          if (abilityScore.equalsIgnoreCase("str") || abilityScore.equalsIgnoreCase("strength")) {
-             return Str;
+             return _str;
          } else if (abilityScore.equalsIgnoreCase("dex") || abilityScore.equalsIgnoreCase("dexterity")) {
-             return Dex;
+             return _dex;
          } else if (abilityScore.equalsIgnoreCase("con") || abilityScore.equalsIgnoreCase("constitution")) {
-             return Con;
+             return _con;
          } else if (abilityScore.equalsIgnoreCase("int") || abilityScore.equalsIgnoreCase("intelligence")) {
-             return Int;
+             return _int;
          } else if (abilityScore.equalsIgnoreCase("wis") || abilityScore.equalsIgnoreCase("wisdom")) {
-             return Wis;
+             return _wis;
          } else if (abilityScore.equalsIgnoreCase("cha") || abilityScore.equalsIgnoreCase("charisma")) {
-             return Cha;
+             return _cha;
          } else {
              return -1;
          }
     }
     
-    void getHP(){
-        
-    }
-    void setHP(){
-        
+    public int getBaseHealth(String baseHealth){
+        if (baseHealth.equalsIgnoreCase("hp") || baseHealth.equalsIgnoreCase("hitpoints") || baseHealth.equalsIgnoreCase("healthpoints")) {
+            return HP;
+        } else {
+            return -1;
+        }
     }
     
     public int getSavingThrow(String savingThrow){
-        
+        if (savingThrow.equalsIgnoreCase("fortsave") || savingThrow.equalsIgnoreCase("fortitudeSave")) {
+             return fortsave;
+         } else if (savingThrow.equalsIgnoreCase("refsave") || savingThrow.equalsIgnoreCase("reflexSave")) {
+             return refsave;
+         } else if (savingThrow.equalsIgnoreCase("willsave") || savingThrow.equalsIgnoreCase("willsave")) {
+             return willsave;
+         } else {
+             return -1;
+         }
     }
-    void getfortsave(){
-        
-    }
-    void setfortsave(){
-        
-    }
-    void getrefsave(){
-        
-    }
-    void setrefsave(){
-        
-    }
-    void getwillsave(){
- 
-    }
-    void setwillsave(){
-        
-    }
-    void getBAB(){
-        
-    }
-    void setBAB(){
-        
-    }
-    void getAC(){
-        
-    }
-    void setAC(){
-        
-    }
-    void getflatAC(){
-        
-    }
-    void setflatAC(){
-        
-    }
-    void gettouchAC(){
-        
-    }
-    void settouchAC(){
-        
-    }
-    void getflattouchAC(){
-        
-    }
-    void setflattouchAC(){
-        
-    }
-    void getcharweight(){
-        
-    }
-    void setcharweight(){
-        
-    }
-    void getitemweight(){
-        
-    }
-    void setitemweight(){
-        
-    }
-    void gettotalweight(){
-        
-    }
-    void settotalweight(){
-        
-    }
-    void getmovespeed(){
-        
-    }
-    void setmovespeed(){
-        
-    }
-    void getnotes(){
     
+    public int getBaseAttackBonus(String baseAttackBonus){
+        if (baseAttackBonus.equalsIgnoreCase("bab") || baseAttackBonus.equalsIgnoreCase("baseattackbonus")) {
+            return BAB;
+        } else {
+            return -1;
+        }
     }
-    void setnotes(){
-        
+
+    public int getWeight(String weight){
+        if (weight.equalsIgnoreCase("characterweight")) {
+            return characterweight;
+        } else {
+            return -1;
+        }
+    }
+    
+    public int getSpeed(String speed){
+        if (speed.equalsIgnoreCase("speed") || speed.equalsIgnoreCase("movementspeed") || speed.equalsIgnoreCase("movement")) {
+            return movespeed;
+        } else {
+            return -1;
+        }
+    }
+    
+    public String getCharacterNotes (String characterNotes){
+        if (characterNotes.equalsIgnoreCase("notes") || characterNotes.equalsIgnoreCase("campaignnotes") || characterNotes.equalsIgnoreCase("characternotes")){
+            return notes;
+        } else {
+            return null;
+        }
     }
 }
     
