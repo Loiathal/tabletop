2b1b4e9a0adf9a5cf55d06e95dee3a835bad959a
zmcanally <zmcanally@IWHQT-385-01.infoworks-tn.com>
2011-11-02 14:26:15 -0500

    Created Equipment.java, which extends from Item.java. Having problems with constructor, so made Equipment.java's members protected. Now can see them from main. Is a problem.
    We'll also need a weapon object, since "1d6" isn't an integer, and I don't think a weapon's damage can really properly be considered a modifier.

--- src/tabletop/Main.java
+++ src/tabletop/Main.java
@@ -8,15 +8,21 @@ import java.util.ArrayList;
 
 class Main {
     public static void main(String[] args) {
-   /*     
-        Feat Feat1 = new Feat("Power Attack", "RAWR", 1, "PHB");
-        Feat1.printFeat();
+   
+        ArrayList acrobaticsModifiers = new ArrayList();
+        Modifier acrobaticsModifier1 = new Modifier(2, "Jump", "Untyped");
+        Modifier acrobaticsModifier2 = new Modifier(2, "Tumble", "Untyped");
+        acrobaticsModifiers.add(acrobaticsModifier1);
+        acrobaticsModifiers.add(acrobaticsModifier2);
+        
+        Feat Acrobatics = new Feat("Acrobatics", "Please only take it once", acrobaticsModifiers, 1, "PHB");
+        Acrobatics.printFeat();
         
         System.out.println("");
         
         Skill Skill1 = new Skill("Jump", 15, "Strength", "yes", "class-skill");
         Skill1.printSkill();
-     */   
+     
         ArrayList kitModifiers = new ArrayList();
         Modifier Modifier1 = new Modifier(2, "Open Lock", "Untyped");
         Modifier Modifier2 = new Modifier(4, "Attack", "Morale");
@@ -24,7 +30,18 @@ class Main {
         kitModifiers.add(Modifier2);
         
         Item masterworkThievesKit = new Item("Tool", "Thieves Kit", kitModifiers, 5, "Helps you open shit.");
-        System.out.println("PRINT");
         masterworkThievesKit.printItem();
+        System.out.println();
+        
+        ArrayList magicSwordModifiers = new ArrayList();
+        Modifier SwordModifier1 = new Modifier(5, "Attack", "Enhancement");
+        Modifier SwordModifier2 = new Modifier(5, "Damage", "Enhancement");
+        Modifier SwordModifier3 = new Modifier(3, "Fire Damage", "Enhancement");
+        magicSwordModifiers.add(SwordModifier1);
+        magicSwordModifiers.add(SwordModifier2);
+        magicSwordModifiers.add(SwordModifier3);
+        
+        Equipment magicSword = new Equipment("Weapon", "+5 flaming longsword", "Hand", magicSwordModifiers, 2, "Hits stuff.");
+        magicSword.printItem();
     }
 }
