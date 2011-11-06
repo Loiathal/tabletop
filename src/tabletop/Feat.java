8df3befa166ca53254b0c0fa8e84fe48759c6b36
zmcanally <zmcanally@IWHQT-385-01.infoworks-tn.com>
2011-11-06 17:25:56 -0600

    Don't use Feat.java form this-- is old.

--- src/tabletop/Feat.java
+++ src/tabletop/Feat.java
@@ -1,59 +1,70 @@
 /*
  * Zachary McAnally
- *
+ * 
  * This object contains a Feat, as well as various information about it.
- *
+ * 
  * Still needs to contain modifier data-- ie. that Weapon Focus gives a +1 to attack, etc.
  */
 
 package tabletop;
 
+import java.util.List;
+import java.util.Iterator;
 
 public class Feat {
     private String name;
     private String description;
-    private Modifier modifier;
+    private List modifierList;
     private int levelTaken;
     private String sourcebook;
+    
+    
    
-    public Feat(String featName, String featDescription, Modifier newModifier, int level, String source)
+    public Feat(String featName, String featDescription, List listOfModifiers, int level, String source)
     {
         name = featName;
         description = featDescription;
         levelTaken = level;
         sourcebook = source;
-        modifier = newModifier;
+        modifierList = listOfModifiers;
     }
-   
+    
     public String getFeatName() {
         return name;
     }
-   
+    
     public String getFeatDescription() {
         return description;
     }
-   
+    
     public int getLevelTaken() {
         return levelTaken;
     }
-   
+    
     public String getSourcebook() {
         return sourcebook;
     }
-   
-    public Modifier getModifier(){
-        return modifier;
+    
+    public List getModifierList(){
+        return modifierList;
     }
-   
+    
     public void printFeat() {
         System.out.println("Name: " + name);
-        System.out.println("Description: " + description);
-        if (modifier != null)
+        if (!modifierList.isEmpty())
         {
-            System.out.println("Modifier: ");
-            modifier.printModifier();
+            System.out.println();
+            System.out.println("Modifiers: ");
+            Iterator modifierIterator = modifierList.iterator();
+            while(modifierIterator.hasNext())
+                {
+                    Modifier modifierObject=(Modifier)modifierIterator.next();
+                    modifierObject.printModifier();
+                }
         }
+        System.out.println();
         System.out.println("Level Taken: " + levelTaken);
         System.out.println("Sourcebook: " + sourcebook);
+        System.out.println("Description: " + description);
     }
 }
