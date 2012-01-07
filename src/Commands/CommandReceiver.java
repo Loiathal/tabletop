0f4581f87d9ec46bf3b4e6daeabf2361e05e2a37
Zachary <Zachary@Loiathal>
2012-01-07 15:14:10 -0600

    Added CommandReceiver.java. This class acts as a receiver, which takes the commands and does the actual work to execute them. The execute method in each Command will call the correct method in the CommandReceiver.java file.

---
+++ src/Commands/CommandReceiver.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Commands;
+
+/**
+ *
+ * @author Zachary
+ */
+public class CommandReceiver {
+    
+    
+    //Begin Bryan's commands
+    public void FieldSet(String field, String value)
+    {
+        
+    }
+    
+    public void FieldChange(String field, String value)
+    {
+        
+    }
+    
+    public void FieldSet(String field, int value)
+    {
+        
+    }
+    
+    public void FieldChange(String field, int value)
+    {
+        
+    }
+    
+    
+    
+    //Begin Zach's commands
+    public String Attack() 
+    {        
+        return "derp";
+    }
+    
+    public int CastSpell()
+    {
+        return 0;
+    }
+    
+    
+    
+    
+    
+    
+}
